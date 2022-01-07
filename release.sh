0a0d42446ece0956da1872baff1d3a3866c1e61f
Harish Bohara <harish.bohara@gmail.com>
2022-01-07 13:08:40 +0530

    initial checkin

---
+++ release.sh
@@ -0,0 +1,7 @@
+mvn -B clean install -Dtag=0.0.1 -Dmaven.test.skip=true release:prepare release:perform -P release
+
+
+# Rollback
+# mvn release:rollback
+# git tag -d 2.0.3
+# git push --delete origin 2.0.2
