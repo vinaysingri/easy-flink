8676383fa277ec0c94852a7711952e873a4c3f7a
Harish Bohara <harish.bohara@gmail.com>
2022-01-07 20:16:40 +0530

    added support to pass kafka source name

--- src/main/java/io/github/devlibx/easy/flink/utils/KafkaSourceHelper.java
+++ src/main/java/io/github/devlibx/easy/flink/utils/KafkaSourceHelper.java
@@ -6,15 +6,38 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.util.List;
 import java.util.Properties;
 
 public class KafkaSourceHelper {
 
-    public static <T> DataStream<T> flink1_12_2_KafkaSource(KafkaSourceConfig config, StreamExecutionEnvironment env, Class<T> cls) {
+    public static <T> DataStream<T> flink1_12_2_KafkaSource(KafkaSourceConfig config, StreamExecutionEnvironment env, String name, String id, Class<T> cls) {
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", config.brokers);
         properties.setProperty("group.id", config.groupId);
-        return env.addSource(new FlinkKafkaConsumer<>(config.topic, new JsonMessageToEventDeserializationSchema<>(cls), properties));
+        if (config.topics == null || config.topics.isEmpty()) {
+            return env
+                    .addSource(
+                            new FlinkKafkaConsumer<>(
+                                    config.topic,
+                                    new JsonMessageToEventDeserializationSchema<>(cls),
+                                    properties
+                            )
+                    )
+                    .name(name)
+                    .uid(id);
+        } else {
+            return env
+                    .addSource(
+                            new FlinkKafkaConsumer<>(
+                                    config.topics,
+                                    new JsonMessageToEventDeserializationSchema<>(cls),
+                                    properties
+                            )
+                    )
+                    .name(name)
+                    .uid(id);
+        }
     }
 
     @Data
@@ -22,6 +45,7 @@ public class KafkaSourceHelper {
     public static class KafkaSourceConfig {
         private String brokers;
         private String topic;
+        private List<String> topics;
         private String groupId;
     }
 }
