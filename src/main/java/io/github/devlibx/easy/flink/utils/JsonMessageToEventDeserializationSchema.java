1ab158b2192884da828a3d341a2716a479f04a54
Harish Bohara <harish.bohara@gmail.com>
2022-02-22 13:50:16 +0530

    Handle error on error in message

--- src/main/java/io/github/devlibx/easy/flink/utils/JsonMessageToEventDeserializationSchema.java
+++ src/main/java/io/github/devlibx/easy/flink/utils/JsonMessageToEventDeserializationSchema.java
@@ -2,6 +2,7 @@ package io.github.devlibx.easy.flink.utils;
 
 import io.gitbub.devlibx.easy.helper.json.JsonUtils;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 
@@ -11,13 +12,20 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 
 @AllArgsConstructor(onConstructor_ = {@Inject})
+@Slf4j
 public class JsonMessageToEventDeserializationSchema<T> implements DeserializationSchema<T>, Serializable {
     private final Class<T> cls;
 
     @Override
     public T deserialize(byte[] message) throws IOException {
-        String line = new String(message, StandardCharsets.UTF_8);
-        return JsonUtils.readObject(line, cls);
+        String line = "";
+        try {
+            line = new String(message, StandardCharsets.UTF_8);
+            return JsonUtils.readObject(line, cls);
+        } catch (Exception e) {
+            log.error("error in deserialization of message={}", line, e);
+            return null;
+        }
     }
 
     @Override
