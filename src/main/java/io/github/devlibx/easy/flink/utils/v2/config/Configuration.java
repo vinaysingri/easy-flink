03c64bfe3d72f9534cea6c2ac9602a75237d885f
Harish Bohara <harish.bohara@gmail.com>
2022-09-09 02:01:12 +0530

    [*] use latest easy lib

--- src/main/java/io/github/devlibx/easy/flink/utils/v2/config/Configuration.java
+++ src/main/java/io/github/devlibx/easy/flink/utils/v2/config/Configuration.java
@@ -1,6 +1,8 @@
 package io.github.devlibx.easy.flink.utils.v2.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.gitbub.devlibx.easy.helper.map.StringObjectMap;
 import lombok.Data;
 
 import java.util.Map;
@@ -11,4 +13,6 @@ public class Configuration {
     private EnvironmentConfig environment;
     private Map<String, SourceConfig> sources;
     private RuleEngineConfig ruleEngine;
+    @JsonProperty("miscellaneous_properties")
+    private StringObjectMap miscellaneousProperties;
 }
