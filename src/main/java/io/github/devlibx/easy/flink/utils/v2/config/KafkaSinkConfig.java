884eaa8af8d2f4c41e04830ce1d9e2c3e4ceb709
Harish Bohara <harish.bohara@gmail.com>
2022-10-20 13:55:20 +0530

    added helper to create a kafka sync

--- src/main/java/io/github/devlibx/easy/flink/utils/v2/config/KafkaSinkConfig.java
+++ src/main/java/io/github/devlibx/easy/flink/utils/v2/config/KafkaSinkConfig.java
@@ -1,12 +1,17 @@
 package io.github.devlibx.easy.flink.utils.v2.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.gitbub.devlibx.easy.helper.map.StringObjectMap;
+import io.github.devlibx.easy.flink.utils.KafkaSourceHelper;
 import lombok.Data;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
 import java.io.Serializable;
 
+@SuppressWarnings("deprecation")
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KafkaSinkConfig implements Serializable {
@@ -31,4 +36,16 @@ public class KafkaSinkConfig implements Serializable {
 
     @JsonProperty("properties")
     private StringObjectMap properties = new StringObjectMap();
+
+    @JsonIgnore
+    public FlinkKafkaProducer<StringObjectMap> getKafkaSinkWithStringObjectMap(StreamExecutionEnvironment env, KafkaSourceHelper.ObjectToKeyConvertor<StringObjectMap> objectMapObjectToKeyConvertor) {
+        return KafkaSourceHelper.flink1_12_2_KafkaSink(
+                KafkaSourceHelper.KafkaSinkConfig.builder()
+                        .brokers(broker)
+                        .topic(topic)
+                        .build(),
+                objectMapObjectToKeyConvertor,
+                StringObjectMap.class
+        );
+    }
 }
